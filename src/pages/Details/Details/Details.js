22d57d9aa05d0548150861ee44ce26c3a19d6f0e
Imonhossin <imonhossin105@gmail.com>
2021-10-20 20:45:27 +0600

    therapy

--- src/pages/Details/Details/Details.js
+++ src/pages/Details/Details/Details.js
@@ -1,22 +1,63 @@
-// import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-
+import {Card, Col, Container, Row} from 'react-bootstrap'
 const Details = () => {
     const { serviceId } = useParams()
-    // const [service, setService] = useState({})
-    // useEffect(() => {
-    //     const url = `public/services.json/details/${serviceId}`
-    //     fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => setService(data))
-    // }, [])
+   
+    const [specific, setSpecific] = useState([])
+
+    const [singleEmployee,setSingleEmployee] = useState({})
+    
+    useEffect(() => {
+        fetch('/specific.json')
+            .then(res => res.json())
+        .then(data=>setSpecific(data.employee))
+    }, [])
+
+    useEffect(() => {
+        const foundEmployee = specific.find(employee => employee.login.id == serviceId)
+        setSingleEmployee(foundEmployee)
+    },[specific])
+
     
     return (
         <div>
             <h2>This is details {serviceId} </h2>
-            {/* {
-                <small>{ service.name}</small>
-            } */}
+            <p>{singleEmployee?.name}</p>
+            
+            <Container>
+                <Row>
+
+                    <Col xl={6}>
+                        <Card>
+                        <Card.Img className='img-fluid' variant="top" src={singleEmployee?.img} />
+
+                        </Card>
+
+                    </Col>
+
+                    <Col xl={6}>
+                    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+            <Card.Title >{singleEmployee?.name}</Card.Title>
+    <Card.Text>
+     {singleEmployee?.description}
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+
+                    </Col>
+
+
+                </Row>
+            </Container>
+
+
+
+
+            
+            
             
         </div>
     );
