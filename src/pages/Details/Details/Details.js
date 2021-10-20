95adb2fa2ee4b5d68477749b39e96677580eb8ff
Imonhossin <imonhossin105@gmail.com>
2021-10-20 21:30:09 +0600

    therapy

--- src/pages/Details/Details/Details.js
+++ src/pages/Details/Details/Details.js
@@ -1,64 +1,46 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import {Card, Col, Container, Row} from 'react-bootstrap'
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import './Details.css'
 const Details = () => {
     const { serviceId } = useParams()
-   
     const [specific, setSpecific] = useState([])
-
     const [singleEmployee,setSingleEmployee] = useState({})
-    
     useEffect(() => {
         fetch('/specific.json')
             .then(res => res.json())
         .then(data=>setSpecific(data.employee))
     }, [])
-
     useEffect(() => {
-        const foundEmployee = specific.find(employee => employee.login.id == serviceId)
+        const foundEmployee = specific.find(employee => employee.id == serviceId)
         setSingleEmployee(foundEmployee)
     },[specific])
-
-    
     return (
         <div>
-            <h2>This is details {serviceId} </h2>
-            <p>{singleEmployee?.name}</p>
-            
+            <h2 className='mt-3'>This is details {serviceId} </h2>
             <Container>
-                <Row>
-
-                    <Col xl={6}>
-                        <Card>
-                        <Card.Img className='img-fluid' variant="top" src={singleEmployee?.img} />
-
+                <Row className='py-5 mx-auto'>
+                    <Col xl={6} lg={6}>
+                        <span className='card-span '>
+                        <Card className='img-fluid h-100'>
+                        <Card.Img className=' h-100' variant="top" src={singleEmployee?.img} />
                         </Card>
-
+                        </span>
                     </Col>
-
-                    <Col xl={6}>
-                    <Card style={{ width: '18rem' }}>
-  <Card.Body>
-            <Card.Title >{singleEmployee?.name}</Card.Title>
-    <Card.Text>
-     {singleEmployee?.description}
-    </Card.Text>
-  </Card.Body>
-</Card>
-
-
+                    <Col xl={6} lg={6}>
+                        <Card className='h-100 ' >
+                    <Card.Body>
+             <Card.Title >
+                   <h3>{singleEmployee?.name}</h3>                 
+             </Card.Title>
+                <Card.Text>
+                <p>{singleEmployee?.description}</p>
+            </Card.Text>
+            </Card.Body>
+                </Card>
                     </Col>
-
-
                 </Row>
             </Container>
-
-
-
-
-            
-            
-            
         </div>
     );
 };
