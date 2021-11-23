f78ac5ab9d019f0af5c5a47640dad267c930969e
imon hossin <imonhossin104@gmail.com>
2021-11-23 21:15:42 +0600

    first

--- src/pages/Firebase/firebase.config.js
+++ src/pages/Firebase/firebase.config.js
@@ -1,9 +1,18 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC_2rtZ7s3YLo6gjaldIFOu181Iu1IAJeI",
+//     authDomain: "physical-therapy-f7c2a.firebaseapp.com",
+//     projectId: "physical-therapy-f7c2a",
+//     storageBucket: "physical-therapy-f7c2a.appspot.com",
+//     messagingSenderId: "857290072639",
+//     appId: "1:857290072639:web:24be1130ad7e979d10ce8a"
+// };
+// export default firebaseConfig;
 const firebaseConfig = {
-    apiKey: "AIzaSyC_2rtZ7s3YLo6gjaldIFOu181Iu1IAJeI",
-    authDomain: "physical-therapy-f7c2a.firebaseapp.com",
-    projectId: "physical-therapy-f7c2a",
-    storageBucket: "physical-therapy-f7c2a.appspot.com",
-    messagingSenderId: "857290072639",
-    appId: "1:857290072639:web:24be1130ad7e979d10ce8a"
-};
-export default firebaseConfig;
+    apiKey: "AIzaSyBZ2xoX4HYfOWXk7j6kcHvfCxetRrNcGNo",
+    authDomain: "healthcare-cebca.firebaseapp.com",
+    projectId: "healthcare-cebca",
+    storageBucket: "healthcare-cebca.appspot.com",
+    messagingSenderId: "75335477762",
+    appId: "1:75335477762:web:f1db5dee39cf7ce60f151b"
+  };
+  export default firebaseConfig;
